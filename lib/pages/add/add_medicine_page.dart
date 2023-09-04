d1ade76ece22d9fbc8eada5f57997f9fe5b79b38
SeoDongSeuk <cast3613@gmail.com>
2023-09-04 18:36:01 +0900

    UI 상수 설정 dart & add page  추가

--- lib/pages/add/add_medicine_page.dart
+++ lib/pages/add/add_medicine_page.dart
@@ -1,12 +1,95 @@
+import 'package:cast/components/cast_constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AddPage extends StatelessWidget {
+class AddPage extends StatefulWidget {
   const AddPage({super.key});
+
+  @override
+  State<AddPage> createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  final _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: const CloseButton(),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: pagePadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '어떤 약이에요?',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              const SizedBox(
+                height: largeSpace,
+              ),
+              Center(
+                child: CircleAvatar(
+                  radius: 40,
+                  child: CupertinoButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      CupertinoIcons.photo_camera_solid,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: largeSpace + regularSpace,
+              ),
+              Text(
+                '약 이름',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              TextFormField(
+                controller: _nameController,
+                maxLength: 20,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.done,
+                style: Theme.of(context).textTheme.bodyLarge,
+                decoration: InputDecoration(
+                  hintText: '복용할 약 이름을 기입해주세요',
+                  hintStyle: Theme.of(context).textTheme.bodyMedium,
+                  contentPadding: textFieldContentPadding,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: submitButtonBoxPadding,
+          child: SizedBox(
+            height: submitButtonHeight,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.titleMedium),
+              child: const Text('다음'),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
